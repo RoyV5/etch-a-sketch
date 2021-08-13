a722ed0ed466eafe37e6b29fd612dc989a87be38
Rodrigo <rodrigoolmospercoco@gmail.com>
2021-08-13 02:20:07 -0400

    Addded resize mechanism, the board will remain a fixed size but increase or decrease the amount of tiles.

--- script.js
+++ script.js
@@ -2,19 +2,38 @@
 
 const mainDiv = document.querySelector('#main-container');
 const childDiv = document.createElement('div');
-childDiv.classList.add('grid-square');
 
 //The loop will append a child to the parent div as many times as specified
 for (let i = 0; i < 100; i++) {
     mainDiv.appendChild(childDiv.cloneNode(true));
 }
 
-//Create the event listener to change the color of the individual grid squares
-let gridSquares = mainDiv.querySelectorAll('.grid-square');
-for (let square of gridSquares) {
-    square.addEventListener('mouseover', function(event) {
-        event.stopPropagation();
-        event.target.style.backgroundColor = 'black';
-    })
+function changeColor(event) {
+    event.stopPropagation();
+    event.target.style.backgroundColor = 'black';
 }
 
+
+//Create the event listener to change the color of the individual grid squares
+let childList = mainDiv.querySelectorAll('#main-container div');
+childList.forEach(square => square.addEventListener('mouseover', changeColor))
+
+
+const clearButton = document.querySelector('#clear-button');
+clearButton.addEventListener('click', function() {
+    childList.forEach(square => square.style.backgroundColor='white')
+})
+
+const resizeButton = document.querySelector('#resize-button');
+resizeButton.addEventListener('click', function(){
+    let tilesPerSide = window.prompt('Enter the ammount of tiles per side you wish your Etch-a-Script to be:');
+    mainDiv.style.gridTemplateColumns = `repeat(${tilesPerSide}, calc(500px / ${tilesPerSide}))`
+    mainDiv.style.gridTemplateRows = `repeat(${tilesPerSide}, calc(500px / ${tilesPerSide}))`
+    childList.forEach(() => mainDiv.removeChild(mainDiv.lastChild));
+    for (let i = 0; i < tilesPerSide * tilesPerSide; i++) {
+        mainDiv.appendChild(childDiv.cloneNode(true))
+    }
+    console.log(childList)
+    childList = mainDiv.querySelectorAll('#main-container div');
+    childList.forEach(square => square.addEventListener('mouseover', changeColor))
+})
