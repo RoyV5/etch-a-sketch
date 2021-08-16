6d304ff2e662d8f4efb63aae090acf380774d7f5
Rodrigo <rodrigoolmospercoco@gmail.com>
2021-08-16 01:26:15 -0400

    Added functionality and refactored code. Now can toggle between rainbow and black states. Changed some CSS styling.

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-let cell = [];
+let penState = 'black'
 
 const gridContainer = document.querySelector('#grid-container');
-console.log(gridContainer.innerHTML)
 
 const clearButton = document.querySelector('#clear-button');
 clearButton.addEventListener('click', clear)
+
+
+function changePenState(state) {
+    clear();
+    penState = state;
+}
+const blackButton = document.querySelector('#black-button');
+blackButton.addEventListener('click', changePenState.bind(null, 'black'));
+//The .bind method "binds" certain parameters to the function that is assigned to the listener.
+//It has to be noted that an event listener function can't be written with parentheses, because then
+//it is processed as if the function is being called, instead of being assigned to the listener. 
+//Because the function is being called within an event listener, the first parameter is assigned to be event.
+//To operate around this, null is passed as the first parameter to indicate that there is no event.
+
+const rainbowButton = document.querySelector('#rainbow-button');
+rainbowButton.addEventListener('click', changePenState.bind(null, 'rainbow'))
+
 
 const resizeButton = document.querySelector('#resize-button');
 resizeButton.addEventListener('click', function(){
@@ -24,20 +40,32 @@ function remove() {
 
 function changeColor(event) {
     event.stopPropagation();
-    event.target.style.backgroundColor = 'black';
-}
-
-function createGrid(tiles) {
-    remove();
-    for (let i = 0; i < (tiles * tiles); i++) {
-        singleTile = 500 / tiles;
-        gridContainer.style.gridTemplateColumns = (`repeat(${tiles}, ${singleTile}px`);
-        gridContainer.style.gridTemplateRows = (`repeat(${tiles}, ${singleTile}px`);
-        cell[i] = document.createElement('div');
-        cell[i].addEventListener('mouseover', changeColor)
-        gridContainer.appendChild(cell[i]);
-        console.log('grid created')
+    switch(penState) {
+        case 'rainbow':
+            ran1 = Math.random() * 255;
+            ran2 = Math.random() * 255;
+            ran3 = Math.random() * 255;
+            event.target.style.backgroundColor = `rgb(${ran1}, ${ran2}, ${ran3})`;
+            break
+        case 'black':
+            event.target.style.backgroundColor = 'rgb(32, 32, 32)';
+            break
     }
 }
 
-createGrid(10);
+function createGrid(tiles) {
+    //Empties the parent node
+    remove();
+    basicTile = document.createElement('div');
+    //Define the space taken by each tile by modifying the gridTemplateColumn/Row CSS style
+    gridContainer.style.gridTemplateColumns = (`repeat(${tiles}, ${500 / tiles}px`);
+    gridContainer.style.gridTemplateRows = (`repeat(${tiles}, ${500 / tiles}px`);
+    for (let i = 0; i < (tiles * tiles); i++) {
+        cell = basicTile.cloneNode(true);
+        cell.addEventListener('mouseover', changeColor);
+        gridContainer.appendChild(cell);
+    }
+}
+
+
+createGrid(25);
