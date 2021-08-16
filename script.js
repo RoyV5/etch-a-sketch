3416acaf7ad9c8b24ec9fb66c4ffc06778b80105
Rodrigo <rodrigoolmospercoco@gmail.com>
2021-08-15 22:31:26 -0400

    Refactored code into a more undertandable, function-oriented version. Functionally the same.

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-//Declatarion of mainDiv and calculation of total grid squares in the etch a script
+let cell = [];
 
-const mainDiv = document.querySelector('#main-container');
-const childDiv = document.createElement('div');
+const gridContainer = document.querySelector('#grid-container');
+console.log(gridContainer.innerHTML)
 
-//The loop will append a child to the parent div as many times as specified
-for (let i = 0; i < 100; i++) {
-    mainDiv.appendChild(childDiv.cloneNode(true));
+const clearButton = document.querySelector('#clear-button');
+clearButton.addEventListener('click', clear)
+
+const resizeButton = document.querySelector('#resize-button');
+resizeButton.addEventListener('click', function(){
+    sideOfGrid = window.prompt('How many tiles per side do you want your grid to be:')
+    createGrid(sideOfGrid); 
+})
+
+function clear() {
+    allTiles = gridContainer.querySelectorAll('#grid-container div')
+    allTiles.forEach(tile => tile.style.backgroundColor='white')
+}
+
+function remove() {
+    allTiles = gridContainer.querySelectorAll('#grid-container div')
+    allTiles.forEach(() => gridContainer.removeChild(gridContainer.lastChild));
 }
 
 function changeColor(event) {
@@ -13,27 +27,17 @@ function changeColor(event) {
     event.target.style.backgroundColor = 'black';
 }
 
-
-//Create the event listener to change the color of the individual grid squares
-let childList = mainDiv.querySelectorAll('#main-container div');
-childList.forEach(square => square.addEventListener('mouseover', changeColor))
-
-
-const clearButton = document.querySelector('#clear-button');
-clearButton.addEventListener('click', function() {
-    childList.forEach(square => square.style.backgroundColor='white')
-})
-
-const resizeButton = document.querySelector('#resize-button');
-resizeButton.addEventListener('click', function(){
-    let tilesPerSide = window.prompt('Enter the ammount of tiles per side you wish your Etch-a-Script to be:');
-    mainDiv.style.gridTemplateColumns = `repeat(${tilesPerSide}, calc(500px / ${tilesPerSide}))`
-    mainDiv.style.gridTemplateRows = `repeat(${tilesPerSide}, calc(500px / ${tilesPerSide}))`
-    childList.forEach(() => mainDiv.removeChild(mainDiv.lastChild));
-    for (let i = 0; i < tilesPerSide * tilesPerSide; i++) {
-        mainDiv.appendChild(childDiv.cloneNode(true))
+function createGrid(tiles) {
+    remove();
+    for (let i = 0; i < (tiles * tiles); i++) {
+        singleTile = 500 / tiles;
+        gridContainer.style.gridTemplateColumns = (`repeat(${tiles}, ${singleTile}px`);
+        gridContainer.style.gridTemplateRows = (`repeat(${tiles}, ${singleTile}px`);
+        cell[i] = document.createElement('div');
+        cell[i].addEventListener('mouseover', changeColor)
+        gridContainer.appendChild(cell[i]);
+        console.log('grid created')
     }
-    console.log(childList)
-    childList = mainDiv.querySelectorAll('#main-container div');
-    childList.forEach(square => square.addEventListener('mouseover', changeColor))
-})
+}
+
+createGrid(10);
